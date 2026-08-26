aae3e7cc6e0f3b4ba4c9b37a3df03afebb72f116
Anup Kushwaha <anupkushwaha916@gmail.com>
2026-08-26 16:40:46 +0530

    Day 3 Comp and Props

---
+++ Day-3/jsx-com/src/About.jsx
@@ -0,0 +1,11 @@
+let About = (props) => {
+    console.log(props);
+    
+  return(
+  <div>
+    <h1>HEllo bro kaise ho</h1>
+  </div>
+  )
+};
+
+export default About;
---
+++ Day-3/jsx-com/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./About";
+
+let App = () =>{
+  return (
+  <div>
+    <About width="500" element={<h1>Hello</h1>}>
+      <h1>Hello</h1>
+    </About>
+  </div>
+  )
+}
+
+export default App;
