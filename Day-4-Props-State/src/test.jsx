145d4280c8ad4d8eadb30f0249b39d9c5918fb1f
Anup Kushwaha <anupkushwaha916@gmail.com>
2026-08-27 17:37:00 +0530

    Day 4 state

---
+++ Day-4-Props-State/src/test.jsx
@@ -0,0 +1,19 @@
+ export const a = () =>{
+    console.log("Hello This is anup");
+    
+}
+
+export const b = () =>{
+    console.log("b");
+    
+}
+export const c = () =>{
+    console.log("c");
+    
+}
+const d = () =>{
+    console.log("d");
+    
+}
+
+export default d;
